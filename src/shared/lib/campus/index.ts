c4c361105f73e029e1cbf94e9d1bb8832a704f7c
Aigiz Tulubaev <flaylay256@gmail.com>
2026-03-28 08:35:17 +0300

    feat: add AppHeader

---
+++ src/shared/lib/campus/index.ts
@@ -0,0 +1,6 @@
+export const CAMPUS_BOARD_BACKGROUND =
+    "linear-gradient(135deg, rgba(39, 94, 254, 0.16), rgba(39, 94, 254, 0.04))";
+
+export const CAMPUS_BOARD_BG_SIZE = "cover";
+
+export const CAMPUS_BOARD_BG_POSITION = "center";
