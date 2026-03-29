e75edc7e5c31ffdce99267c615a55ebee7ece973
Aigiz Tulubaev <flaylay256@gmail.com>
2026-03-29 19:55:53 +0300

    fix: update project card types

--- src/entities/project/index.ts
+++ src/entities/project/index.ts
@@ -1,4 +1,4 @@
 import ProjectCard from "./ui/ProjectCard";
 
 export { ProjectCard };
-export type { ProjectCardData } from "./model/ProjectCardDate";
+export type { ProjectCardData } from "./model/types";
