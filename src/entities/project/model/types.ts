6e87b2756644443b6f7ffafbcd080a2ab20992da
Anastasiya <uecnheca@gmai.com>
2026-04-02 18:31:12 +0300

    feat: добавить страницу 'Мои проекты' с карточками проектов

--- src/entities/project/model/types.ts
+++ src/entities/project/model/types.ts
@@ -8,3 +8,15 @@ export type ProjectCardData = {
     meta: string;
     members: string;
 };
+
+export type MyProjectCardData = {
+    id: number;
+    date: string;
+    weekday: string;
+    title: string;
+    destination: string;
+    subtitle: string;
+    description: string;
+    meta: string;
+    members: string;
+};
--- src/widgets/ContentFilters/index.ts
+++ src/widgets/ContentFilters/index.ts
@@ -1,3 +1,4 @@
 import ContentFilters from "./ui/ContentFilters";
 
 export { ContentFilters };
+export { default as ProjectsToogleGroup } from "./ui/ProjectsToogleGroup";
