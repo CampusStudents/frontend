f9cd26a22af6adc67fb0532abe5901d0db5e0fb4
Anastasiya <uecnheca@gmai.com>
2026-04-05 20:06:29 +0300

    feat: добавлена страница кандидатов

--- src/entities/project/index.ts
+++ src/entities/project/index.ts
@@ -3,4 +3,7 @@ import ProjectCard from "./ui/ProjectCard";
 export { ProjectCard };
 
 export { default as MyProjectCard } from "./ui/MyProjectCard";
+export { default as CandidateCard } from "./ui/CandidateCard";
 export type { MyProjectCardData } from "./model/types";
+export type { ProjectCardData } from "./model/types";
+export type { CandidatesCardData } from "./model/types";
--- src/widgets/ContentFilters/index.ts
+++ src/widgets/ContentFilters/index.ts
@@ -2,3 +2,4 @@ import ContentFilters from "./ui/ContentFilters";
 
 export { ContentFilters };
 export { default as ProjectsToggleGroup } from "./ui/ProjectsToggleGroup";
+export { default as ContentFindFilters } from "./ui/ContentFindFilters";
